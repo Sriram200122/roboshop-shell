53587d6d499c7b38c48d2d333ef90f927541c60e
sriram <munukutla2001@gmail.com>
2023-10-04 18:19:15 +0530

    project

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -3,7 +3,7 @@ yum install nginx -y
 echo -e "\e[33m Removing default Nginx content\e[0m"
 cd /usr/share/nginx/html
 rm -rf *
-echo -e "\e[33m Download New content to Nginx\e[0m"
+echo -e "\e[33m Download New Content to Nginx\e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip frontend.zip
 rm -rf frontend.zip
