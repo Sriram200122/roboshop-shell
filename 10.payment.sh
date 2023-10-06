a88645c9bc7be551d41beca837247e5c32be03ac
sriram <munukutla2001@gmail.com>
2023-10-06 14:24:47 +0530

    project

--- 10.payment.sh
+++ 10.payment.sh
@@ -10,9 +10,9 @@ curl -O https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 unzip payment.zip
 echo -e "\e[32m Downloading dependencies and building application to shipping server\e[0m"
 pip3.6 install -r requirements.txt
-echo -e "\e[32m creating shipping service file\e[0m"
+echo -e "\e[32m creating payment service file\e[0m"
 cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
-echo -e "\e[32m Enabling and starting the shipping service\e[0m"
+echo -e "\e[32m Enabling and starting the payment service\e[0m"
 systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+systemctl enable payment
+systemctl restart payment
