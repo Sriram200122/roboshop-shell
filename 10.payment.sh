17974058bce39fc116af7692d0a5c17948e0cd50
sriram <munukutla2001@gmail.com>
2023-10-06 14:14:03 +0530

    project

--- 10.payment.sh
+++ 10.payment.sh
@@ -4,12 +4,13 @@ echo -e "\e32m Adding user and location\e[0m"
 useradd roboshop
 mkdir /app
 cd /app
+rm -rf *
 echo -e "\e[32m Downloading new app content and dependencies to payment server\e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 unzip payment.zip
 pip3.6 install -r requirements.txt
 echo -e "\e[32m creating payment service file\e[0m"
-cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
+cp /root/roboshop-shell/payment.service  /etc/systemd/system/payment.service
 echo -e "\e[32m Enabling and starting the payment service\e[0m"
 systemctl daemon-reload
 systemctl enable payment
